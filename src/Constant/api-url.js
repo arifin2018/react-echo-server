b87a1c6261fb8ee0b9981b6744d7527ffd9c1110
arifinlenna <arifin@lenna.ai>
2023-09-03 05:24:39 +0700

    add API_URL

--- src/Constant/api-url.js
+++ src/Constant/api-url.js
@@ -1,6 +1,7 @@
 // const API_URL = 'https://f6fa-103-171-161-143.ngrok.io/api/login';
 // const API_URL = 'https://td-be.vercel.app';
-const API_URL = 'http://54.254.178.234:8012/api';
+// const API_URL = 'http://54.254.178.234:8012/api';
+const API_URL = 'https://backend.arifinportfolio.my.id/api';
 // const API_URL = 'http://localhost:8012/api';
 
 export default API_URL
