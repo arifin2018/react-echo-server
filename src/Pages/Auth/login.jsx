02e08f7904485d2e47dac5547712b065fb9dba39
arifinlenna <arifin@lenna.ai>
2023-09-07 09:44:53 +0700

    delete not use login

--- src/Pages/Auth/login.jsx
+++ src/Pages/Auth/login.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import Card from '../../Components/card'
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
