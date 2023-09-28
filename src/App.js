b2c11e887bb3f4787d7600749d9db70d8ad589f5
arifinlenna <arifin@lenna.ai>
2023-09-29 05:26:36 +0700

    update laravel echo server

--- src/App.js
+++ src/App.js
@@ -8,27 +8,27 @@ import { getCookie } from './Helpers/Cookie';
 import { MessagesRecoil, UserChatRecoil } from './Helpers/Recoil';
 import API_URL from './Constant/api-url';
 import { getImageStorage } from './Constant/firebase';
+import Echo from 'laravel-echo'
 
 function App() {
   const [messages, setMessages] = useRecoilState(MessagesRecoil);
   const [userChat, setUserChat] = useRecoilState(UserChatRecoil);
 
   useEffect(()=>{
-    
-    // Pusher.logToConsole = true;
-    var channel = new Pusher('72c7cc74868f610f8163', {
-        cluster: 'mt1',
-        forceTLS: true,
-        encrypted: true,
-        authEndpoint:`${API_URL}/broadcasting/auth`,
-        auth: {
-            headers: {
-                Authorization: `Bearer ${getCookie('access_token')}`,
-                Accept: 'application/json',
-            },
+    window.io = require('socket.io-client');
+    window.Echo = new Echo({
+      broadcaster: 'socket.io',
+      host: 'http://localhost:6011',
+      authEndpoint:`${API_URL}/broadcasting/auth`,
+      key: "682cb8acab2f22e12728f0f349648bc6",
+      withCredentials: true,
+      auth: {
+        headers: {
+            Authorization: `Bearer ${getCookie('access_token')}`
         },
-    }).subscribe(`private-messageChat-${userChat.id}`);
-    channel.bind('private.sendMessage', async (data,error) => {
+      },
+    });
+    window.Echo.private(`messageChat-${userChat.id}`).listen('.private.sendMessage',async (data,error) => {
       if (data.dataMessage.type == 'image') {
         console.log(data.dataMessage);
         const resultMessage = await Promise.resolve(getImageStorage(data.dataMessage.message))
@@ -41,12 +41,50 @@ function App() {
           ...old,data.dataMessage
         ]);
       }else{
+        data.dataMessage = {
+          ...data.dataMessage,
+          created_at:new Date().toISOString(),
+        }
         setMessages((old) => [
           ...old,data.dataMessage
         ]);
       }
-    });
-},[userChat])
+    })
+
+    // Pusher.logToConsole = true;
+    // var channel = new Pusher('72c7cc74868f610f8163', {
+    //     cluster: 'mt1',
+    //     forceTLS: true,
+    //     encrypted: true,
+    //     authEndpoint:`${API_URL}/broadcasting/auth`,
+    //     auth: {
+    //         headers: {
+    //             Authorization: `Bearer ${getCookie('access_token')}`,
+    //             Accept: 'application/json',
+    //         },
+    //     },
+    // }).subscribe(`private-messageChat-${userChat.id}`);
+    // channel.bind('private.sendMessage', async (data,error) => {
+    //   if (data.dataMessage.type == 'image') {
+    //     console.log(data.dataMessage);
+    //     const resultMessage = await Promise.resolve(getImageStorage(data.dataMessage.message))
+    //     data.dataMessage = {
+    //       ...data.dataMessage,
+    //       message: resultMessage,
+    //       created_at:new Date().toISOString(),
+    //     }
+    //     setMessages((old) => [
+    //       ...old,data.dataMessage
+    //     ]);
+    //   }else{
+    //     setMessages((old) => [
+    //       ...old,data.dataMessage
+    //     ]);
+    //   }
+    // });
+
+  },[userChat])
+
 
   return (
     <div className="App">
